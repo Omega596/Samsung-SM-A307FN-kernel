8c663a089ab65cb860d40e7090105108e22df5e1
Marc Zyngier <maz@kernel.org>
2020-09-01 10:52:33 +0100

    HID: core: Sanitize event code and type when mapping input
    commit 35556bed836f8dc07ac55f69c8d17dce3e7f0e25 upstream.
    
    When calling into hid_map_usage(), the passed event code is
    blindly stored as is, even if it doesn't fit in the associated bitmap.
    
    This event code can come from a variety of sources, including devices
    masquerading as input devices, only a bit more "programmable".
    
    Instead of taking the event code at face value, check that it actually
    fits the corresponding bitmap, and if it doesn't:
    - spit out a warning so that we know which device is acting up
    - NULLify the bitmap pointer so that we catch unexpected uses
    
    Code paths that can make use of untrusted inputs can now check
    that the mapping was indeed correct and bail out if not.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hid/hid-input.c
+++ drivers/hid/hid-input.c
@@ -1069,6 +1069,18 @@ static void hidinput_configure_usage(struct hid_input *hidinput, struct hid_fiel
 mapped:
 	/* Mapping failed, bail out */
 	if (!bit)
+<<<<<<< HEAD
+=======
+		return;
+
+	if (device->driver->input_mapped &&
+	    device->driver->input_mapped(device, hidinput, field, usage,
+					 &bit, &max) < 0) {
+		/*
+		 * The driver indicated that no further generic handling
+		 * of the usage is desired.
+		 */
+>>>>>>> de801a7d3228... HID: core: Sanitize event code and type when mapping input
 		return;
 
 	if (device->driver->input_mapped && device->driver->input_mapped(device,
